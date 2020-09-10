254a10f6bd7160762fff8d7edf221f3ef6050655
Isaac Alves <isaac.alves0720@gmail.com>
2020-09-10 20:43:59 -0300

    first commit

---
+++ 17-ordem.decrescente.js
@@ -0,0 +1,3 @@
+for(var i = 10; i > -1; i--){ //ordem decrescente JS
+    console.log(i);
+}
---
+++ 19-imc.js
@@ -0,0 +1,5 @@
+var peso = prompt("Digite o peso: ","");
+var altura = prompt("Digite a altura: ","");
+
+imc = peso / (altura ** 2);
+window.alert(imc);
