d2aa61f786537488039dc0f825359b1af8a4f26c
Isaac Alves <isaac.alves0720@gmail.com>
2020-09-19 02:51:14 -0300

    second commit

---
+++ assets/public/scripts/js/0-O que é o JavaScript (JS).js
@@ -0,0 +1,11 @@
+/* 
+Servidor e Cliente
+
+A internet é uma rede de computadores e obtemos arquivos através da Web. Em desenvolvimento para a Web sempre separamos dois setores: 
+a parte do servidor e a parte do cliente. 
+*/
+
+/*
+JavaScript (JS)
+
+*/
